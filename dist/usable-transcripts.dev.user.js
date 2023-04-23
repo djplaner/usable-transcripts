9c9e4a8b9e41b3b6a800bb020207da01a0bfc484
David Jones <davidthomjones@gmail.com>
2023-04-23 12:16:07 +1000

    Workign again?

---
+++ dist/usable-transcripts.dev.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        usable-transcripts
+// @description Exploration into providing usable transcripts
+// @namespace   https://djon.es/
+// @version     0.0.1-alpha
+// @homepage    https://github.com/djplaner/usable-transcripts#readme
+// @author      David Jones
+// @updateURL   https://github.com/djplaner/usable-canvas-userscript/raw/main/dist/usable-canvas-userscript.user.js
+// @downloadURL https://github.com/djplaner/usable-canvas-userscript/raw/main/dist/usable-canvas-userscript.user.js
+// @supportURL  https://github.com/djplaner/usable-canvas-userscript/issues
+// @resource    css file:///C:/Users/s2986288/code/usable-transcripts/dev/usable-transcripts.css
+// @require     file:///C:/Users/s2986288/code/usable-transcripts/dev/usable-transcripts.js
+// @match       https://*/courses/*
+// @run-at      document-idle
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
